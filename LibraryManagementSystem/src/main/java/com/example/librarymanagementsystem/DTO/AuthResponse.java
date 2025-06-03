28eadcd33a69f3366501a592237a40eba5cff236
saher mohamed <120128502+SaherMohamed250@users.noreply.github.com>
2025-06-03 13:07:32 -0700

    Add files via upload

---
+++ LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/DTO/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.example.librarymanagementsystem.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String message;
+}
